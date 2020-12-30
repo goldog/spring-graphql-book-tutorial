e68619f2db9265c9db3ec37c9461f70d42cb8b29
goldog <goldog83@gmail.com>
2020-12-30 15:35:30 +0900

    최초 commit

---
+++ src/main/java/com/mctak/springgraphqlbooktutorial/SpringGraphqlBookTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.mctak.springgraphqlbooktutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGraphqlBookTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGraphqlBookTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mctak/springgraphqlbooktutorial/SpringGraphqlBookTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mctak.springgraphqlbooktutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGraphqlBookTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
